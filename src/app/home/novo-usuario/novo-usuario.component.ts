cef32c92346a6b466ad34c1bbc3c2e1ddc3dc851
Eberthyn Marques <eberthyn.marques@saude.gov>
2021-10-07 17:42:09 -0300

    Linkando dados de Novo Usuário

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,14 +1,34 @@
+import { NovoUsuarioService } from './novo-usuario.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  styleUrls: ['./novo-usuario.component.css'],
 })
 export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    // tslint:disable-next-line: no-shadowed-variable
+    private NovoUsuarioService: NovoUsuarioService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: ['']
+    });
+  }
+
+  cadastrar() {
+    const novoUsuario = this.novoUsuarioForm.getRawValue();
+    console.log(novoUsuario);
+  }
 
 }
