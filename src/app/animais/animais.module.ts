d0a8b42cee3b666043321e63c696335e9198581b
Eberthyn Marques <eberthyn@gmail.com>
2021-10-15 18:07:00 -0300

    Lista de Animais c/ Validações de Rotas

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -5,12 +5,14 @@ import { CommonModule } from '@angular/common';
 import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
+import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
 
 
 @NgModule({
   declarations: [
     ListaAnimaisComponent,
-    AnimalComponent
+    AnimalComponent,
+    GradeFotosAnimaisComponent
   ],
   imports: [
     CommonModule,
